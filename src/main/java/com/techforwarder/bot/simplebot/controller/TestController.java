d4b1d3846df1ef7997d5dc625f61cce86f80f5f8
kimseak-dev <kimseakphon@gmail.com>
2019-10-18 09:07:10 +0700

    commit

---
+++ src/main/java/com/techforwarder/bot/simplebot/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.techforwarder.bot.simplebot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return  "hello";
+    }
+}
